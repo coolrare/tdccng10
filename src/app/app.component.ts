06e44b95ac5ada54c4a656099af7b641236ad31b
Will <doggy.huang@gmail.com>
2020-07-10 17:36:13 +0800

    示範 ngStyle 與 ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   keyword = 'TEST';
 
+  // TEST
   doSearch(value: string): void {
     this.keyword = value;
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -10,6 +10,7 @@ export class HeaderComponent implements OnInit {
   sitename = 'Hello World';
   subtitle = '記載著 <strong>Will</strong> 在網路世界的學習心得與技術分享';
   sitelogo = '/assets/images/logo.png';
+  counter = 12;
 
   constructor() { }
 
@@ -21,6 +22,7 @@ export class HeaderComponent implements OnInit {
 
   changeTitle($event: MouseEvent): void {
     console.log($event);
+    this.counter++;
     if ($event.ctrlKey) {
       this.sitename = 'The Will Will Web';
     }
