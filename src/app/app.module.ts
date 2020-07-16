a223a45171bbb5ed8ea8c6539faae77838baac37
Will <doggy.huang@gmail.com>
2020-07-16 15:50:23 +0800

    示範自訂 Pipe 寫法

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MaskPipe } from './mask.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    MaskPipe
   ],
   imports: [
     BrowserModule,
